7a8444dc9b42450fa4251feb17b1c7401648a1f7
lihh <lihh@mapbar.com>
2018-09-19 18:14:36 +0800

    chg 第一ci

---
+++ MyApplication/app/src/main/java/com/sucetech/yijiamei/view/ConBluthView.java
@@ -0,0 +1,14 @@
+package com.sucetech.yijiamei.view;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * Created by lihh on 2018/9/18.
+ */
+
+public class ConBluthView extends LinearLayout {
+    public ConBluthView(Context context) {
+        super(context);
+    }
+}
